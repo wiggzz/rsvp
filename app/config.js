9c4ba0381e2e664f86c4d76b2d119763cd54f5f6
Will James <jameswt@gmail.com>
2015-08-11 23:16:38 +1000

    oh, i didnt push. try setting headers from node

--- app/config.js
+++ app/config.js
@@ -2,5 +2,7 @@ module.exports = {
   mailgunApiUrl: 'https://api.mailgun.net/v3',
   mailgunAccount: process.env.MAILGUN_DOMAIN,
   mailgunApiKey: process.env.MAILGUN_API_KEY,
-  sendToEmail: process.env.EMAIL
+  sendToEmail: process.env.EMAIL,
+  accessControllAllowOrigin: '*',
+  accessControllAllowMethods: 'POST'
 };
