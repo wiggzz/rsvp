2bdd16fb2fa324cfa9d077d722194a28a9d25fdf
Will James <jameswt@gmail.com>
2015-08-11 22:40:14 +1000

    removed config template.

--- app/config.template.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mailgunApiUrl: 'https://api.mailgun.net/v3',
-  mailgunAccount: 'mg.example.com',
-  mailgunApiKey: process.env.MAILGUN_API_KEY,
-  sendToEmail: 'example@example.com',
-  accessControllAllowOrigin: null,
-  accessControllAllowMethods: null
-};
